17a224b1b095508ff5abff6e11137c3bb1fa0512
cisuno7 <cisuno7@hotmail.com>
2023-03-15 12:21:22 -0300

    animação da tela de carregamento feita

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,21 +1,62 @@
+import 'dart:async';
+import 'package:bipixapp/pages/login.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class Loading extends StatelessWidget {
-  const Loading({super.key});
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    //  animação da logo esmaecendo
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 3000),
+    );
+    _animation = Tween<double>(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut),
+    );
+
+    // Inicia a animação da logo
+    _animationController.forward();
+
+    // Aguarda 2 segundos antes de abrir a tela de login
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Login()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Center(
-        child: SizedBox(
-          width: 200,
-          height: 200,
-          child: Image.asset('assets/images/bipixLogo.png'),
+        child: FadeTransition(
+          opacity: _animation,
+          child: SizedBox(
+            width: 200,
+            height: 200,
+            child: Image.asset('assets/images/bipixLogo.png'),
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
