dded6165068f8f77f36d32bd6c1ae3252753d881
cisuno7 <cisuno7@hotmail.com>
2023-05-09 22:13:34 -0300

    criado novo widget do zoom

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:bipixapp/pages/select_bet_value.dart';
 import 'package:bipixapp/pages/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:bipixapp/widgets/damas.dart';
+import 'package:flutter_zoom_videosdk/native/zoom_videosdk.dart';
+
 
 void main() {
   runApp(const MainApp());
