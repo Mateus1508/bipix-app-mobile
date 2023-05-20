798299827eb4727325066a1763cfd460ee8cabea
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2023-05-20 01:28:57 -0300

    :add: mudanças na infoBar

--- lib/widgets/info_bar.dart
+++ lib/widgets/info_bar.dart
@@ -11,46 +11,15 @@ class InfoBar extends StatelessWidget implements PreferredSizeWidget {
     String cash = "400,00";
 
     return AppBar(
-      title: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 1),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.shade300, blurRadius: 1, spreadRadius: 3),
-          ],
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            Icon(
-              Icons.gamepad_sharp,
-              color: Colors.blue,
-              size: 15,
-            ),
-            SizedBox(
-              width: 3,
-            ),
-            Text(
-              "Bipix game",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 12,
-              ),
-            )
-          ],
-        ),
-      ),
       actions: [
         Padding(
-          padding: const EdgeInsets.only(right: 60),
+          padding: const EdgeInsets.only(right: 30),
           child: Center(
             child: Container(
               padding: const EdgeInsets.only(right: 7, left: 1),
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(50),
                 boxShadow: [
                   BoxShadow(
                       color: Colors.grey.shade300,
